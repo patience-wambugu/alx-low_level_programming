14faa3284840f3757d0bc650af91fc4f476123cb
patience-wambugu <106748969+patience-wambugu@users.noreply.github.com>
2022-07-11 20:32:46 +0300

    update print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-		if (i == (n - 1))
-			printf("%d", a[n - 1]);
-	printf("\n");
+	i = 0;
+	while (i < n)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
+	}
+	putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,7 +11,6 @@ void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
-void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif
