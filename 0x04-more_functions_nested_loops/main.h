ec68fb0d4d044c10c1d46875f7753c88a5f1c66e
patience-wambugu <106748969+patience-wambugu@users.noreply.github.com>
2022-07-11 13:25:39 +0300

    Add: print diagonal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,6 +14,8 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+
 int _putchar(char c);
 void print_triangle(int size);
 
